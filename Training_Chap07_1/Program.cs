52006cf62ebbb4498c0746bb7a1f1191ef52c6cf
천승용 <127080149+202395031@users.noreply.github.com>
2024-05-09 14:13:12 +0900

    Add files via upload

---
+++ Training_Chap07_1/Program.cs
@@ -0,0 +1,39 @@
+﻿class Point
+{
+    private int x;
+    private int y;
+    public void set(int x, int y) 
+    {
+        this.x = x; 
+        this.y = y;
+    }
+
+    public void showPoint()
+    {
+        Console.WriteLine($"({x}, {y})");
+    }
+}
+class ColorPoint : Point
+{
+    private String color;
+    public void setColor(string x)
+    {
+        this.color = x;
+    }
+    public void showColorPoint() { Console.Write(color); showPoint(); }
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Point p = new Point();
+        p.set(1,2);
+        p.showPoint();
+
+        ColorPoint cp = new ColorPoint();
+        cp.set(3, 4);
+        cp.setColor("red");
+        cp.showColorPoint();
+    }
+}
